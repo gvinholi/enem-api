a60a3034fd7d249a6833f2f7f9728e6ed575b5f1
gvinholi <gabrielvinholi@gmail.com>
2026-06-23 18:46:57 -0300

    feat: implement answer validation system

--- src/components/Questao.jsx
+++ src/components/Questao.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
 import ReactMarkdown from "react-markdown";
 
 function Questao({ questao }) {
+    const [resposta, setResposta] = useState(null);
+    const [corrigido, setCorrigido] = useState(false);
+
   return (
     <div>
       <h2>
@@ -13,11 +17,28 @@ function Questao({ questao }) {
 
       <div>
         {questao.alternativas.map((alt) => (
-          <label key={alt.letter} style={{ display: "block" }}>
-            <input type="radio" name={questao.id} />
+          <label key={alt.letter}
+           style={{ display: "block" }}>
+            <input 
+                type="radio" 
+                name={questao.id}
+                onChange={() => setResposta(alt.letter)}
+            />
             <strong>{alt.letter})</strong> {alt.text}
           </label>
         ))}
+
+        <button onClick={() => setCorrigido(true)}>
+            Corrigir
+        </button>
+
+        {corrigido && (
+            <p>
+                {resposta === questao.gabarito
+                ? "✔ Correto"
+                : "❌ Errado"}
+            </p>
+        )}
       </div>
     </div>
   );
